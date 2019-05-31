51f903e8ba9a6ae1ba0300541459d98b892454eb
Jeremy <jeremychan1887@gmail.com>
2019-05-31 18:45:33 +0800

    js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,44 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+
+	//1、判断输入是否合法，非法则返回NaN
+	if (isValid(inputs) === false) {
+		return NaN;
+	}
+	//2、输入合法，返回行程产生的价格
+	return taxiPrice(inputs);
+	
 };
+//计算价格
+function taxiPrice(inputs) {
+	let distance = inputs.distance;
+	let parkTime = inputs.parkTime;
+	
+	//1、里程产生的运价
+	//起步价 distance<=2需要的费用
+	var sum = 6;
+	//如果distance>8，需要的费用
+	if (distance > 8) {
+		sum += (8-2)*0.8 + (distance-8)*0.8*(1+0.5);
+	} 
+	//如果2<distance<=8，需要的费用
+	if (distance>2 && distance<=8) {
+		sum += (distance-2)*0.8
+	}
+	
+	//2、加上停车产生的费用
+	sum += parkTime*0.25;
+		
+	return Math.round(sum);
+}
+//判断输入合法性
+function isValid(inputs) {
+	//如果两者有一个不是数字，则为非法
+	if (typeof inputs.distance!=='number' || typeof inputs.parkTime!=='number') {
+		return false;
+	}
+	//如果两个数字有一个小于0，则为非法
+	if (inputs.distance<0 || inputs.parkTime<0) {
+		return false;
+	}
+	return true;
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,28 +1,47 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    it('within 2km and parking' , function() {
+    it('invalid inputs' , function() {
+        let inputs = {distance:'1',parkTime:5};
+        let summary = main(inputs);
+        let expected = NaN;
+        expect(summary).toEqual(expected);
+    }); 
+	it('invalid inputs' , function() {
+        let inputs = {distance:-5,parkTime:5};
+        let summary = main(inputs);
+        let expected = NaN;
+        expect(summary).toEqual(expected);
+    }); 
+	it('between 2km and 8km and parking' , function() {
+        let inputs = {distance:6.5,parkTime:7}; //6+0.25*7+(6.5-2)*0.8=11.35
+        let summary = main(inputs);
+        let expected = 11;
+        expect(summary).toEqual(expected);
+    }); 
+	
+	it('within 2km and parking' , function() {
         let inputs = {distance:1,parkTime:5};
         let summary = main(inputs);
         let expected = 7;
         expect(summary).toEqual(expected);
-    });
+    }); 
     it('between 2km and 8km and parking' , function() {
         let inputs = {distance:7,parkTime:10};
         let summary = main(inputs);
         let expected = 13;
         expect(summary).toEqual(expected);
-    });
+    }); 
     it('more than 8km and parking' , function() {
         let inputs = {distance:10,parkTime:20};
         let summary = main(inputs);
         let expected = 18;
         expect(summary).toEqual(expected);
-    });
-    it('more than 8km and no parking' , function() {
-        let inputs = {distance:15,parkTime:0};
+    }); 
+    it('more than 8km and no parking' , function() { 
+        let inputs = {distance:15,parkTime:0}; 
         let summary = main(inputs);
         let expected = 19;
         expect(summary).toEqual(expected);
-    });
+    }); //19.2
 });
